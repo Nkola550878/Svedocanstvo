9ed176b6515f8980ff86dfac4d4619ae04453d5f
Ucenik <Ucenik@DESKTOP-OGGEBNA>
2024-12-05 17:46:35 +0100

    Add project files.

---
+++ Svedocanstvo/Osoba.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Svedocanstvo
+{
+    partial class Osoba
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.id = new System.Windows.Forms.TextBox();
+            this.ime = new System.Windows.Forms.TextBox();
+            this.prezime = new System.Windows.Forms.TextBox();
+            this.adresa = new System.Windows.Forms.TextBox();
+            this.jmbg = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.uloga = new System.Windows.Forms.TextBox();
+            this.osobaID = new System.Windows.Forms.Label();
+            this.osobaIme = new System.Windows.Forms.Label();
+            this.osobaPrezime = new System.Windows.Forms.Label();
+            this.osobaAdresa = new System.Windows.Forms.Label();
+            this.osobaJMBG = new System.Windows.Forms.Label();
+            this.osobaEmail = new System.Windows.Forms.Label();
+            this.osobaPassword = new System.Windows.Forms.Label();
+            this.osobaUloga = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnUnesi = new System.Windows.Forms.Button();
+            this.btnObrisi = new System.Windows.Forms.Button();
+            this.btnSledeci = new System.Windows.Forms.Button();
+            this.btnPoslednji = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // id
+            // 
+            this.id.Location = new System.Drawing.Point(316, 95);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(246, 20);
+            this.id.TabIndex = 0;
+            // 
+            // ime
+            // 
+            this.ime.Location = new System.Drawing.Point(316, 121);
+            this.ime.Name = "ime";
+            this.ime.Size = new System.Drawing.Size(246, 20);
+            this.ime.TabIndex = 1;
+            // 
+            // prezime
+            // 
+            this.prezime.Location = new System.Drawing.Point(316, 147);
+            this.prezime.Name = "prezime";
+            this.prezime.Size = new System.Drawing.Size(246, 20);
+            this.prezime.TabIndex = 2;
+            // 
+            // adresa
+            // 
+            this.adresa.Location = new System.Drawing.Point(316, 173);
+            this.adresa.Name = "adresa";
+            this.adresa.Size = new System.Drawing.Size(246, 20);
+            this.adresa.TabIndex = 3;
+            // 
+            // jmbg
+            // 
+            this.jmbg.Location = new System.Drawing.Point(316, 199);
+            this.jmbg.Name = "jmbg";
+            this.jmbg.Size = new System.Drawing.Size(246, 20);
+            this.jmbg.TabIndex = 4;
+            // 
+            // email
+            // 
+            this.email.Location = new System.Drawing.Point(316, 225);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(246, 20);
+            this.email.TabIndex = 5;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(316, 251);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(246, 20);
+            this.password.TabIndex = 6;
+            // 
+            // uloga
+            // 
+            this.uloga.Location = new System.Drawing.Point(316, 277);
+            this.uloga.Name = "uloga";
+            this.uloga.Size = new System.Drawing.Size(246, 20);
+            this.uloga.TabIndex = 7;
+            // 
+            // osobaID
+            // 
+            this.osobaID.AutoSize = true;
+            this.osobaID.Location = new System.Drawing.Point(255, 95);
+            this.osobaID.Name = "osobaID";
+            this.osobaID.Size = new System.Drawing.Size(15, 13);
+            this.osobaID.TabIndex = 8;
+            this.osobaID.Text = "id";
+            // 
+            // osobaIme
+            // 
+            this.osobaIme.AutoSize = true;
+            this.osobaIme.Location = new System.Drawing.Point(255, 121);
+            this.osobaIme.Name = "osobaIme";
+            this.osobaIme.Size = new System.Drawing.Size(23, 13);
+            this.osobaIme.TabIndex = 9;
+            this.osobaIme.Text = "ime";
+            // 
+            // osobaPrezime
+            // 
+            this.osobaPrezime.AutoSize = true;
+            this.osobaPrezime.Location = new System.Drawing.Point(255, 147);
+            this.osobaPrezime.Name = "osobaPrezime";
+            this.osobaPrezime.Size = new System.Drawing.Size(43, 13);
+            this.osobaPrezime.TabIndex = 10;
+            this.osobaPrezime.Text = "prezime";
+            // 
+            // osobaAdresa
+            // 
+            this.osobaAdresa.AutoSize = true;
+            this.osobaAdresa.Location = new System.Drawing.Point(255, 173);
+            this.osobaAdresa.Name = "osobaAdresa";
+            this.osobaAdresa.Size = new System.Drawing.Size(39, 13);
+            this.osobaAdresa.TabIndex = 11;
+            this.osobaAdresa.Text = "adresa";
+            // 
+            // osobaJMBG
+            // 
+            this.osobaJMBG.AutoSize = true;
+            this.osobaJMBG.Location = new System.Drawing.Point(255, 199);
+            this.osobaJMBG.Name = "osobaJMBG";
+            this.osobaJMBG.Size = new System.Drawing.Size(29, 13);
+            this.osobaJMBG.TabIndex = 12;
+            this.osobaJMBG.Text = "jmbg";
+            // 
+            // osobaEmail
+            // 
+            this.osobaEmail.AutoSize = true;
+            this.osobaEmail.Location = new System.Drawing.Point(255, 225);
+            this.osobaEmail.Name = "osobaEmail";
+            this.osobaEmail.Size = new System.Drawing.Size(31, 13);
+            this.osobaEmail.TabIndex = 13;
+            this.osobaEmail.Text = "email";
+            // 
+            // osobaPassword
+            // 
+            this.osobaPassword.AutoSize = true;
+            this.osobaPassword.Location = new System.Drawing.Point(255, 251);
+            this.osobaPassword.Name = "osobaPassword";
+            this.osobaPassword.Size = new System.Drawing.Size(52, 13);
+            this.osobaPassword.TabIndex = 14;
+            this.osobaPassword.Text = "password";
+            // 
+            // osobaUloga
+            // 
+            this.osobaUloga.AutoSize = true;
+            this.osobaUloga.Location = new System.Drawing.Point(255, 277);
+            this.osobaUloga.Name = "osobaUloga";
+            this.osobaUloga.Size = new System.Drawing.Size(33, 13);
+            this.osobaUloga.TabIndex = 15;
+            this.osobaUloga.Text = "uloga";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 320);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "<<";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(232, 320);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(313, 320);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "izmeni";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnUnesi
+            // 
+            this.btnUnesi.Location = new System.Drawing.Point(394, 320);
+            this.btnUnesi.Name = "btnUnesi";
+            this.btnUnesi.Size = new System.Drawing.Size(75, 23);
+            this.btnUnesi.TabIndex = 19;
+            this.btnUnesi.Text = "unesi";
+            this.btnUnesi.UseVisualStyleBackColor = true;
+            // 
+            // btnObrisi
+            // 
+            this.btnObrisi.Location = new System.Drawing.Point(475, 320);
+            this.btnObrisi.Name = "btnObrisi";
+            this.btnObrisi.Size = new System.Drawing.Size(75, 23);
+            this.btnObrisi.TabIndex = 20;
+            this.btnObrisi.Text = "obrisi";
+            this.btnObrisi.UseVisualStyleBackColor = true;
+            // 
+            // btnSledeci
+            // 
+            this.btnSledeci.Location = new System.Drawing.Point(556, 320);
+            this.btnSledeci.Name = "btnSledeci";
+            this.btnSledeci.Size = new System.Drawing.Size(75, 23);
+            this.btnSledeci.TabIndex = 21;
+            this.btnSledeci.Text = ">";
+            this.btnSledeci.UseVisualStyleBackColor = true;
+            this.btnSledeci.Click += new System.EventHandler(this.btnSledeci_Click);
+            // 
+            // btnPoslednji
+            // 
+            this.btnPoslednji.Location = new System.Drawing.Point(637, 320);
+            this.btnPoslednji.Name = "btnPoslednji";
+            this.btnPoslednji.Size = new System.Drawing.Size(75, 23);
+            this.btnPoslednji.TabIndex = 22;
+            this.btnPoslednji.Text = ">>";
+            this.btnPoslednji.UseVisualStyleBackColor = true;
+            this.btnPoslednji.Click += new System.EventHandler(this.btnPoslednji_Click);
+            // 
+            // Osoba
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 453);
+            this.Controls.Add(this.btnPoslednji);
+            this.Controls.Add(this.btnSledeci);
+            this.Controls.Add(this.btnObrisi);
+            this.Controls.Add(this.btnUnesi);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.osobaUloga);
+            this.Controls.Add(this.osobaPassword);
+            this.Controls.Add(this.osobaEmail);
+            this.Controls.Add(this.osobaJMBG);
+            this.Controls.Add(this.osobaAdresa);
+            this.Controls.Add(this.osobaPrezime);
+            this.Controls.Add(this.osobaIme);
+            this.Controls.Add(this.osobaID);
+            this.Controls.Add(this.uloga);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.jmbg);
+            this.Controls.Add(this.adresa);
+            this.Controls.Add(this.prezime);
+            this.Controls.Add(this.ime);
+            this.Controls.Add(this.id);
+            this.Name = "Osoba";
+            this.Text = "Osoba";
+            this.Load += new System.EventHandler(this.Osoba_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox id;
+        private System.Windows.Forms.TextBox ime;
+        private System.Windows.Forms.TextBox prezime;
+        private System.Windows.Forms.TextBox adresa;
+        private System.Windows.Forms.TextBox jmbg;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox uloga;
+        private System.Windows.Forms.Label osobaID;
+        private System.Windows.Forms.Label osobaIme;
+        private System.Windows.Forms.Label osobaPrezime;
+        private System.Windows.Forms.Label osobaAdresa;
+        private System.Windows.Forms.Label osobaJMBG;
+        private System.Windows.Forms.Label osobaEmail;
+        private System.Windows.Forms.Label osobaPassword;
+        private System.Windows.Forms.Label osobaUloga;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnUnesi;
+        private System.Windows.Forms.Button btnObrisi;
+        private System.Windows.Forms.Button btnSledeci;
+        private System.Windows.Forms.Button btnPoslednji;
+    }
+}
---
+++ Svedocanstvo/Osoba.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace Svedocanstvo
+{
+    public partial class Osoba : Form
+    {
+        public Osoba()
+        {
+            InitializeComponent();
+        }
+
+        int brojReda = 0;
+        DataTable podaci = new DataTable();
+
+        //string connectionString = @"Server=your_server_name; Database=your_database_name; Integrated Security=True;";
+
+        private void Osoba_Load(object sender, EventArgs e)
+        {
+            PopuniText();
+        }
+
+        void PopuniText()
+        {
+
+            string connectionString = "Server=DESKTOP-OGGEBNA\\SQLEXPRESS; database=ednevnik; integrated Security=SSPI;";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from osoba", connection);
+
+            adapter.Fill(podaci);
+
+            id.Text = ((int)podaci.Rows[brojReda][0]).ToString();
+            ime.Text = (string)podaci.Rows[brojReda][1];
+            prezime.Text = (string)podaci.Rows[brojReda][2];
+            adresa.Text = (string)podaci.Rows[brojReda][3];
+            jmbg.Text = (string)podaci.Rows[brojReda][4];
+            email.Text = (string)podaci.Rows[brojReda][5];
+            password.Text = (string)podaci.Rows[brojReda][6];
+            uloga.Text = ((int)podaci.Rows[brojReda][7]).ToString();
+        }
+
+        private void btnSledeci_Click(object sender, EventArgs e)
+        {
+            brojReda++;
+            PopuniText();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            brojReda--;
+            PopuniText();
+        }
+
+        private void btnPoslednji_Click(object sender, EventArgs e)
+        {
+            brojReda = podaci.Rows.Count - 1;
+            PopuniText();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            brojReda = 0;
+            PopuniText();
+        }
+    }
+}
